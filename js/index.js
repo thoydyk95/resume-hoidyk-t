55c7693bbf7e8356537fa7bb148ffb9339e6d244
Tetiana <thoydyk@gmail.com>
2017-08-02 16:31:01 +0300

    fghfdh
    rgrdghdr

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
