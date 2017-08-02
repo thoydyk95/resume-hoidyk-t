10751120b79b4133a6535508927bd62420f64df5
Tetiana <thoydyk@gmail.com>
2017-08-02 16:35:21 +0300

    hghg
    hthth

--- js/index.js
+++ js/index.js
@@ -5,6 +5,21 @@ addEventListener("load", function() {
 		function hideURLbar() {
 			window.scrollTo(0, 1);
 		}
+(function() {
+					[].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
+						new CBPFWTabs( el );
+					});
+				})();
+
+$(document).ready(function() {
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			});
+jQuery(document).ready(function($) {
+		$(".scroll ").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
