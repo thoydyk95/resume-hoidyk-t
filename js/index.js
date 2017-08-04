c75ce7e03071ba6c2c4ab132ed94348c84be28da
Tetiana <thoydyk@gmail.com>
2017-08-04 17:37:51 +0300

    htrh
    rtgre

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-(function() {
-					[].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
-						new CBPFWTabs( el );
-					});
-				})();
-
-$(document).ready(function() {
-			$().UItoTop({ easingType: 'easeOutQuart' });
-			});
-jQuery(document).ready(function($) {
-		$(".scroll ").click(function(event){		
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-		});
-	});
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
