f3fb7bc4a63ae54219b9caa7b83053e7521038be
Tetiana <thoydyk@gmail.com>
2017-08-04 17:33:03 +0300

    bgfb
    fbfb

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-addEventListener("load", function() {
-			setTimeout(hideURLbar, 0);
-		}, false);
-
-		function hideURLbar() {
-			window.scrollTo(0, 1);
-		}
 (function() {
 					[].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
 						new CBPFWTabs( el );
